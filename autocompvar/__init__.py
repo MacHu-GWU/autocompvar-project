68107fc2e0314da9ce459da696145876a1957471
MacHu-GWU <husanhe@gmail.com>
2017-02-24 14:47:47 -0500

    2017-02-24

--- autocompvar/__init__.py
+++ autocompvar/__init__.py
@@ -6,6 +6,10 @@ __short_description__ = "make your data importable"
 __license__ = "MIT"
 __author__ = "Sanhe Hu"
 
-from .helpers import create_data_script
-from .metadata import gen_code
-from .name_convention import to_variable_name
+try:
+    from .helpers import create_data_script
+    from .metadata import gen_code
+    from .name_convention import to_variable_name
+except ImportError:
+    pass
+    
